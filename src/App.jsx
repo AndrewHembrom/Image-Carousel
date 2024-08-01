321685f0f7ad06edac1b99fc08a7eeafe4d4d271
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-08-01 21:52:04 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import { Carousel } from './components/Carousel'
+import { slides } from './images/CarouselImages.json'
+import "./App.css"
 
 function App() {
 
   return (
-      <div>
-        <Carousel />
+      <div className='App'>
+      <Carousel data={ slides } />
       </div>
   )
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs'
 import "./Carousel.css";
 
-export const Carousel = () => { 
+export const Carousel = ({ data }) => { 
+    const [slide, setSlide] = useState(0);
+
+    const nextSlide = () => { 
+        setSlide(slide === data.length - 1 ? 0 : slide + 1)
+    }
+
+    const prevSlide = () => { 
+        setSlide(slide === 0 ? data.length-1 : slide - 1)
+    }
+
     return (
-        <div>
-            Carousel
+        <div className='carousel'>
+            <BsArrowLeftCircleFill onClick={ prevSlide } className="arrow arrow-left" />
+            {data.map((image, index) => {
+                return (
+                    <img src={image.src} alt={image.alt} key={index} className={slide===index ? 'slide' : 'slide slide-hidden'}/>
+                )
+            })}
+            <BsArrowRightCircleFill onClick={ nextSlide } className="arrow arrow-right"/>
+            <span className='indicators'>
+                {data.map((_, index) => { 
+                    return (
+                        <button key={index} onClick={() => { setSlide(index)}} className={slide===index ? 'indicator' : 'indicator indicator-hidden'}></button>
+                    )
+                })}
+            </span>
         </div>
     )
 }
